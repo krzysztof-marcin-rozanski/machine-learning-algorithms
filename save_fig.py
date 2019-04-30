7eb5cfc024faabd299f8c1dc2d2d867e3d411edf
Krzysztof Różański <krzysztof.marcin.rozanski@gmail.com>
2019-04-30 16:48:00 +0200

    minor extension update

--- save_fig.py
+++ save_fig.py
@@ -9,13 +9,13 @@ import os
 import matplotlib.pyplot as plt
 
 
-def save_fig(output_dir, fig_id, tight_layout=True):
+def save_fig(output_dir, fig_id, tight_layout=True, fig_extension="png"):
     """ Saves image """
     path = os.path.join(output_dir, "images")
     if not os.path.isdir(path):
         os.mkdir(path)
-    path = os.path.join(path, fig_id + ".png")
-    print(" Saving figure ", fig_id)
+    path = os.path.join(path, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
     if tight_layout:
         plt.tight_layout()
-    plt.savefig(path, format='png', dpi=300)
+    plt.savefig(path, format=fig_extension, dpi=300)
