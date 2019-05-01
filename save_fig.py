f9643bc488d0d76cd5b5ffa8c6aa4b36efa904ef
Krzysztof Różański <krzysztof.marcin.rozanski@gmail.com>
2019-05-01 20:18:36 +0200

    KR-6 update return

--- save_fig.py
+++ save_fig.py
@@ -15,7 +15,7 @@ def save_fig(output_dir, fig_id, tight_layout=True, fig_extension="png"):
     if not os.path.isdir(path):
         os.mkdir(path)
     path = os.path.join(path, fig_id + "." + fig_extension)
-    print("Saving figure", fig_id)
     if tight_layout:
         plt.tight_layout()
     plt.savefig(path, format=fig_extension, dpi=300)
+    return print("Saving figure", fig_id)
