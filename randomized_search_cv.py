6aecfa2fb2b32a225414deeffa2335b6d0a7877b
Krzysztof Różański <krzysztof.marcin.rozanski@gmail.com>
2019-07-06 17:53:00 +0200

    KR-12 randomized_search

--- randomized_search_cv.py
+++ randomized_search_cv.py
@@ -1,31 +1,32 @@
 """
-Created on Thr July 04 2019
+Created on Thr July 05 2019
 
 @author: krzysztof_rozanski
 """
 
 # Initial packages
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
 import time
 import pandas as pd
 
 
-def grid_search_cv(model, X_df_train, y_train, param_grid, cv, scoring, n_jobs=4):
+def randomized_search_cv(model, X_df_train, y_train, param_distribs, n_iter, cv, scoring, n_jobs=4):
     """ Performs calibration of optimal set of hyperparameters with respect to chosen score,
-    using cross-validation. Evaluates all the possible combinations of hyperparameter values.
+    using cross-validation. Evaluates given number of random combinations by selecting a random
+    value for each hyperparameter at every iteration.
     Pleas use sklearns metrics.SCORERS.keys() command to select desired score.
     """
     start = time.time()
     print("- Starting:")
-    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=cv, scoring=scoring, return_train_score=True,
-                               n_jobs=n_jobs)
-    grid_search.fit(X_df_train, y_train)
+    rnd_search = RandomizedSearchCV(estimator=model, param_distributions=param_distribs,
+                                    n_iter=n_iter, cv=cv, scoring=scoring, return_train_score=True, n_jobs=n_jobs)
+    rnd_search.fit(X_df_train, y_train)
     end = time.time()
-    print("- Finished:", end-start)
-    cvres = grid_search.cv_results_
+    print("- Finished:", end - start)
+    cvres = rnd_search.cv_results_
     print(f"{scoring}_train     ", f"{scoring}_test      ", "Param:      ")
     for mean_score_train, mean_score_test, params in zip(cvres["mean_train_score"],
                                                          cvres["mean_score_test"],
                                                          cvres["params"]):
         print(mean_score_train, mean_score_test, params)
-    return grid_search, pd.DataFrame(cvres)
+    return rnd_search, pd.DataFrame(cvres)
