6c6a48efb1bfcdb5d048ae8b0ad7e3a7707db966
Krzysztof Różański <krzysztof.marcin.rozanski@gmail.com>
2019-07-07 11:19:24 +0200

    KR-7 change of path function for more universal one

--- file_pather.py
+++ file_pather.py
@@ -6,12 +6,13 @@ Created on Wed May 01 2019
 
 # Initial packages
 import os
+import sys
 
 
 def file_pather(file_path=None):
     """ Changes the current working directory """
     if not file_path:
-        os.chdir(os.path.dirname(os.path.realpath(__file__)))
+        os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))
     else:
         os.chdir(file_path)
     return print(os.getcwd())
